c8471fa38a306599b9177ebf8541f2de3d01120a
Nick Mistretta <nickmistretta9@gmail.com>
2018-04-09 17:05:32 -0400

    Initial commit

---
+++ WebScraper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace WebScraper
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
